8af0990828dc023e721854e0bf577cbf6118ba96
jorgequiguango <Jorge Luis>
2020-04-30 20:32:51 -0500

    Fix set buy price product with dot and comma in ProductsEditor

--- src/main/java/com/openbravo/pos/inventory/ProductsEditor.java
+++ src/main/java/com/openbravo/pos/inventory/ProductsEditor.java
@@ -501,7 +501,8 @@ public final class ProductsEditor extends javax.swing.JPanel implements EditorRe
 
         myprod[3] = m_jCodetype.getSelectedItem();
         myprod[4] = m_jName.getText();
-        myprod[5] = Formats.CURRENCY.parseValue(m_jPriceBuy.getText());
+        //Added by Jorge Luis to replace comma from dot
+        myprod[5] = Formats.CURRENCY.parseValue(m_jPriceBuy.getText().replace(".", ","));
         myprod[6] = pricesell;
         myprod[7] = m_CategoryModel.getSelectedKey();
         myprod[8] = taxcatmodel.getSelectedKey();
@@ -940,8 +941,8 @@ public final class ProductsEditor extends javax.swing.JPanel implements EditorRe
 
         if (!reportlock) {
             reportlock = true;
-
-            Double dPriceBuy = readCurrency(m_jPriceBuy.getText());
+            //Added by Jorge Luis to replace comma from dot
+            Double dPriceBuy = readCurrency(m_jPriceBuy.getText().replace(".", ","));
             Double dPriceSell = (Double) pricesell;
 
             if (dPriceBuy == null || dPriceSell == null) {
@@ -974,8 +975,8 @@ public final class ProductsEditor extends javax.swing.JPanel implements EditorRe
 
         if (!reportlock) {
             reportlock = true;
-
-            Double dPriceBuy = readCurrency(m_jPriceBuy.getText());
+            //Added by Jorge Luis to replace comma from dot
+            Double dPriceBuy = readCurrency(m_jPriceBuy.getText().replace(".", ","));
             Double dPriceSell = (Double) pricesell;
 
             if (dPriceBuy == null || dPriceSell == null) {
@@ -993,8 +994,8 @@ public final class ProductsEditor extends javax.swing.JPanel implements EditorRe
 
         if (!reportlock) {
             reportlock = true;
-
-            Double dPriceBuy = readCurrency(m_jPriceBuy.getText());
+            //Added by Jorge Luis to replace comma from dot
+            Double dPriceBuy = readCurrency(m_jPriceBuy.getText().replace(".", ","));
             Double dMargin = readPercent(m_jmargin.getText());
 
             if (dMargin == null || dPriceBuy == null) {
